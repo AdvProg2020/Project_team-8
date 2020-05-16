e6a7cf56bbbcf74c5367814061f13435db2435ff
R.erfan24 <r.erfan24@outlook.com>
2020-05-16 15:53:12 +0430

    finish Login Register on View

--- Java/candyMarket/src/main/java/View/ViewException.java
+++ Java/candyMarket/src/main/java/View/ViewException.java
@@ -4,6 +4,9 @@ public class ViewException extends Exception {
     public static ViewException invalidNumber(){
         return new ViewException("please enter valid number");
     }
+    public static ViewException invalidEmailFormat(){
+        return new ViewException("Please enter valid mail");
+    }
     public ViewException (String msg){
         super(ConsoleDesign.RED+"Error : "+ ConsoleDesign.WHITE+msg);
     }
