99f0c0efca1aeda50ff6aa48063f994410149154
R.erfan24 <r.erfan24@outlook.com>
2020-05-17 03:34:53 +0430

    Finalize Registering (on View)

--- Java/candyMarket/src/main/java/Controller/LoginOrRegisterManaging.java
+++ Java/candyMarket/src/main/java/Controller/LoginOrRegisterManaging.java
@@ -1,9 +1,12 @@
 package Controller;
 
 
+import java.util.ArrayList;
+
 public class LoginOrRegisterManaging {
-    public static boolean register() {
-        return false;
+
+    public static String register(ArrayList<String> info) {
+        return null;
     }
 
     public static int login() { //nemidunm ye chi pas bede ke oun enum moon ro to View taqir bedim //@Arash_ST
