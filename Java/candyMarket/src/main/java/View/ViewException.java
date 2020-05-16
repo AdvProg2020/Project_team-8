d85c1618619f9b5cb2f1394e372e0e895a56b371
R.erfan24 <r.erfan24@outlook.com>
2020-05-17 03:39:55 +0430

    Finalize Registering (on View)

--- Java/candyMarket/src/main/java/View/ViewException.java
+++ Java/candyMarket/src/main/java/View/ViewException.java
@@ -4,6 +4,9 @@ public class ViewException extends Exception {
     public static ViewException invalidNumber(){
         return new ViewException("please enter valid number");
     }
+    public static ViewException cantComment(){
+        return new ViewException("you should buy the good at first");
+    }
     public static ViewException invalidEmailFormat(){
         return new ViewException("Please enter valid mail");
     }
