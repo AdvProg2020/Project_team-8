fa5ba34b65aa61414b8c4530c30f957d7012ce22
R.erfan24 <r.erfan24@outlook.com>
2020-06-25 01:53:27 +0430

    add login/logout method

--- Java/candyMarket/src/main/java/Model/UserHandler.java
+++ Java/candyMarket/src/main/java/Model/UserHandler.java
@@ -7,11 +7,31 @@ public  class UserHandler {
         public static Seller currentSeller;
         public static Manager currentManager;
         public static boolean isLogeIn(){
-                if(currentUser == null)
-                        return false;
-                return true;
+                return currentUser != null;
         }
         public static void start(){
                 currentCart = new Cart();
+        }
+
+        public static void loggingIn(User user) {
+                currentUser = user;
+                switch (currentUser.getType()) {
+                        case BUYER:
+                                currentBuyer = (Buyer)user;
+                                break;
+                        case SELLER:
+                                currentSeller = (Seller)user;
+                                break;
+                        case MANAGER:
+                                currentManager = (Manager)user;
+                                break;
+                }
+        }
+
+        public static void loggingOut() {
+                currentUser = null;
+                currentManager = null;
+                currentBuyer = null;
+                currentSeller = null;
         }
 }
